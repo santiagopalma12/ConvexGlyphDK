b35e9a29312165f71f6c301d15d23e930787b94a
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-11-19 00:18:18 -0500

    feat: Renderizado básico de polígonos en pantalla

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from src.letter_mesh import generate_polygon_mesh
 
 # Configuración inicial
 pygame.init()
@@ -9,6 +10,10 @@ pygame.display.set_caption("ConvexGlyph - Prototipo")
 clock = pygame.time.Clock()
 
 def main():
+    # Generar una forma de prueba
+    poly = generate_polygon_mesh('A', 100)
+    poly = [(x + 400, y + 300) for x, y in poly] # Centrar
+
     while True:
         screen.fill((30, 30, 30))
         
@@ -16,6 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        
+        # Dibujar polígono
+        pygame.draw.polygon(screen, (0, 255, 100), poly, 2)
         
         pygame.display.flip()
         clock.tick(60)
