ed6888607ae431df2cc67a2db8daf492c0f803e9
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-11-19 00:20:38 -0500

    feat: Algoritmo de detección de punto en polígono (Ray Casting)

--- src/geometry.py
+++ src/geometry.py
@@ -11,3 +11,21 @@ def sub(v1, v2):
 
 def add(v1, v2):
     return (v1[0] + v2[0], v1[1] + v2[1])
+
+def is_point_in_polygon(point, polygon):
+    # Algoritmo simple de Ray Casting (Even-Odd rule)
+    x, y = point
+    n = len(polygon)
+    inside = False
+    p1x, p1y = polygon[0]
+    for i in range(n + 1):
+        p2x, p2y = polygon[i % n]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+    return inside
